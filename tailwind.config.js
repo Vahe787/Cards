3757633ff09c386a81046c9713b7be283e29667e
Vahe <ghazaryanv787@gmail.com>
2022-05-13 11:34:49 +0400

    add(Tailwind): added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      height: {
+        500: "497px",
+      },
+      spacing: {
+        54: "190px",
+        62: "248px",
+      },
+    },
+  },
+  plugins: [],
+};
