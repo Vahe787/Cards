89cecfc1e85d11e95e239bcad8c54eca2304666d
Vahe <ghazaryanv787@gmail.com>
2022-05-13 14:12:26 +0400

    change(Instructions): changed text

--- src/components/menu/Instructions.js
+++ src/components/menu/Instructions.js
@@ -5,8 +5,8 @@ const Instructions = () => {
     <div className="border-l-2 border-gray-100">
       <p className="mt-44 p-3 font-bold w-62 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
         Press the "Add Card" button to add the new Card. use the "Sort Cards"
-        button to sort the Cards by the increase. Press an X icon on the top
-        right to delete item
+        button to sort the Cards by the increase. Press an delete icon on the
+        top right to delete item
       </p>
     </div>
   );
